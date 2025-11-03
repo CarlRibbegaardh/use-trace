8d16e104f7707e3efd71405d465ca44134f29335
Carl Ribbegårdh <carl.ribbegardh+git@gmail.com>
2025-11-03 11:20:26 +0100

    Test coverage

--- packages/auto-tracer-plugin-vite/tests/setupTests.ts
+++ packages/auto-tracer-plugin-vite/tests/setupTests.ts
@@ -1,7 +1,4 @@
 import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
 
 // Clean up DOM between tests to prevent test pollution
-afterEach(() => {
-  cleanup();
-});
+afterEach(() => {});
